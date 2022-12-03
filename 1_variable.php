148e9c136749ff72ed40b2e06f92cc0f9623079f
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-12-03 16:27:20 +0600

    first commit

---
+++ 1_variable.php
@@ -0,0 +1,42 @@
+<?php 
+// echo "I am PHP";
+
+// single line comment (//)
+
+/*  double line comment
+    line 1
+    line 2
+    line 3
+*/   
+
+// variable declare
+
+// $firstName = "Md.";
+// $middleName = "Khairuzzaman";
+// $lastName= "Hridoy";
+
+// echo $firstName.' '.$middleName.' '.$lastName ; 
+
+// $x=12;
+// $y=6;
+
+// print $x+$y;  
+// print $x-$y; 
+// print $x*$y; 
+// print $x/$y; 
+
+/*
+    DATA TYPES -->
+    1. Number [ Interger(12,13,5) ; Float(12.5,13.675,5.6)] 
+    2. String ['Hridoy','Dhaka','Khulna','Messi']
+    3. Boolean [ true,false ]
+    4. Array
+    5. Object
+    6. Resources
+    7. Null 
+*/
+
+$cars = array("Volvo","B&W","Hino","Hundy","Sakura");
+print_r($cars[1]);
+
+ ?>

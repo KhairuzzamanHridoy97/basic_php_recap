8a3209ba22c468cc4e257bce5ce04ad39c87d4b8
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-12-03 23:52:24 +0600

    conditional

---
+++ 2_conditional_loops.php
@@ -0,0 +1,69 @@
+<?php 
+// if..else
+
+// $age =21 ;
+// if($age >= 18 )
+// echo "You are an adult";
+// else 
+// echo "You are not adult"; 
+
+// $color = "blue";
+// if($color == "blue")
+// echo "The color is  blue";
+// else
+// echo "The color is not blue " ;
+
+//  if..elseif..else 
+
+/*
+$color = "red";
+if($color == "green")
+print "The color is not red. It is green" ;
+elseif($color=="blue")
+print "The color is not red. It is blue";
+elseif($color == "aqua")
+print "The color is is not red . It is aqua" ;
+elseif($color == "yellow")
+print "This is red color";
+else 
+print "Color is not found"; */
+
+// Switch Case 
+
+/*
+$color = "black" ;
+
+switch($color){
+    case "blue":
+        echo " This is not black . This is blue";
+        break ;
+    case "green":
+        echo "This is not black . This is green";
+        break;
+    case "pink":
+        echo " This is black color . ";
+        break;
+    default:
+        print "404 !  Color is not found!!";
+} */
+
+$capital = "Dhaka";
+
+switch($capital){
+    case "Khulna":
+        echo "Khulna is not capital city .";
+        break;
+    case "Barisal":
+        echo "Barisal is not capital city . ";
+        break;
+    case "Rangpur":
+        echo "Dhaka is our capital city";
+        break;
+    default:
+        echo "Capital not found!";
+
+}
+
+
+
+?>

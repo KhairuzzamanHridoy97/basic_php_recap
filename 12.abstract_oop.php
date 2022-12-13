704cc4be68ef5a02bfb8091dafd3e788cc317b63
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-12-13 14:01:44 +0600

    another commit

--- 12.abstract_oop.php
+++ 12.abstract_oop.php
@@ -5,33 +5,46 @@ An abstract class is a class that contains at least one abstract method. An abst
 */
 
 // Parent class
-abstract class Car {
-    public $name;
-    public function __construct($name) {
-      $this->name = $name;
-    }
-    abstract public function intro() : string;
-  }
 
+    abstract class Car{
+        public $name;
+        public function __construct($name)
+        {
+            $this-> name=$name;
+        }
+        abstract public function intro(): string;
+    };
 
 // Child classes
-class Ena extends Car {
+
+// class Ena extends Car {
+//     public $made_in;
+
+//     public function intro() : string {
+//       return "Choose $this->made_in quality! I'm an $this->name!";
+//     }
+
+//     public function myownMethod()
+//     {
+//         return "This is my own method";
+//     }
+//   }
+
+
+class Ena extends Car{
     public $made_in;
-
-    public function intro() : string {
-      return "Choose $this->made_in quality! I'm an $this->name!";
-    }
-
-    public function myownMethod()
+    public function intro(): string
     {
-        return "This is my own method";
+        return "Choose $this->made_in in quality! I'm an $this->name !"; // "this-name" ta holo abstract 
     }
-  }
-
+    public function myownMethod(){
+        return "This is our own method";
+    }
+}
 
 // Create objects from the child classes
-$Ena = new Ena("Ena");
-$Ena->made_in = "German";
+$Ena = new Ena("Ena"); // Ekhane  $Ena holo object 
+$Ena->made_in = "German"; // made_in asche  Ena namok Chil_class theke 
 print_r($Ena);
 echo $Ena->intro();
 echo "<br>";

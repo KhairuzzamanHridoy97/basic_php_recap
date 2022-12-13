f79c7f73e43791db4e9ae434187417ef6aea6a32
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-12-14 00:46:30 +0600

    date time

---
+++ 17.date_time.php
@@ -0,0 +1,13 @@
+<?php
+
+    echo date_default_timezone_set('Asia/Dhaka');
+    echo '<br/>';
+    echo date('l-d-m-y');
+    echo '<br/>';
+    echo date('h:i:s: a');
+    echo '<br/>';
+    echo date('d/m/y');
+    echo '<br/>';
+    echo date_default_timezone_get();
+
+?>
